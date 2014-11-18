e275098b8097390111110e6af24e988ebdb1209e
Julien Guy <jguy@cvrsvc04.(none)>
2014-11-18 15:21:29 -0800

    implementation of targets.get_templates

--- py/desisim/targets.py
+++ py/desisim/targets.py
@@ -5,6 +5,14 @@ Utility functions for working with simulated targets
 import yaml
 import os
 import numpy as np
+import sys
+import fitsio
+import random
+import pylab
+import desisim.cosmology
+import desisim.interpolation
+import astropy.units 
+import math
 
 def sample_nz(objtype, n):
     """
@@ -116,21 +124,104 @@ def get_templates(wave, objtype, redshift):
     
     Returns 2D array of spectra[len(objtype), len(wave)]
     """
-    
-    nspec = len(redshift)
 
-    #- Allow objtype to be a single string instead of an array of strings
-    if is_instance(objtype, str):
-        objtype = [objtype,] * nspec
-    else:
-        assert len(redshift) == len(objtype)
-        
     #- Look for templates in
     #- $DESI_LRG_TEMPLATES, $DESI_ELG_TEMPLATES, etc.
     #- If those envars aren't set, default to most recent version number in
     #- $DESI_SPECTRO_TEMPLATES/{objtype}_templates/{version}/*.fits[.gz]
     
-    #- Randomly subsample those templates to get nspec of them
-        
-    raise NotImplementedError
+    #- Randomly subsample those templates to get nspec of them    
+
     
+    #- Allow objtype to be a single string instead of an array of strings
+    if isinstance(objtype, str):
+        objtype = [objtype,] * len(redshift)
+    else:
+        assert len(redshift) == len(objtype)
+    
+    #- Store the list of known objtype and associated template filename
+    known_filenames_for_objtypes={}
+    try :
+        known_filenames_for_objtypes["LRG"]=os.getenv("DESI_LRG_TEMPLATES")
+        known_filenames_for_objtypes["ELG"]=os.getenv("DESI_ELG_TEMPLATES")
+        known_filenames_for_objtypes["STAR"]=os.getenv("DESI_STAR_TEMPLATES")
+        #known_filenames_for_objtypes["QSO"]=os.getenv("unknown")
+    except :
+        print sys.exc_info()
+        print "ERROR in targets.get_templates when getting environments variables"
+        sys.exit(12) # need a better exception here 
+    
+    #- allocate spectra
+    spectra=np.zeros((len(objtype), len(wave)))
+    
+    #- open only once each type to minimize IO
+    for obj in set(objtype) :
+        
+        try :
+            filename=known_filenames_for_objtypes[obj]
+        except KeyError, e:
+            print "ERROR in targets.get_templates, unknown objtype '%s'"%obj
+            print "known objtypes are",known_filenames_for_objtypes.keys
+            raise e
+        
+        print filename
+        file=fitsio.FITS(filename)
+        header = file[0].read_header()
+
+        if header["CTYPE1"] != 'WAVE-WAV' or header["CUNIT1"] != 'Angstrom' \
+                or  header["BUNIT"] != 'erg/s/cm2/A' \
+                or  header["FLUXUNIT"] != 'erg/s/cm2/A' :
+            print "ERROR in targets.get_templates, sorry, something has changed in the template files since implementation, need to review this function"
+            sys.exit(12) # need a better exception here 
+                
+        loglam=header["CDELT1"]*np.arange(header["NAXIS1"])+header["CRVAL1"]
+        file_restframe_wave=10**loglam
+        number_of_spectra_in_file=header["NAXIS2"]
+        print loglam.size,number_of_spectra_in_file
+        
+        #- now loop on requested entries
+        for obj_req, z, index in zip(objtype,redshift,np.arange(redshift.size)) :
+            
+            #- check match objtype of file we have opened 
+            if obj_req != obj :
+                continue
+            
+            #- take one random row in file
+            entry=int(random.random()*number_of_spectra_in_file)           
+            
+            #- read only one entry of the file
+            restframe_spectrum=file[0][entry:entry+1,:][0]
+            # print obj,z,entry,restframe_spectrum.shape
+            
+            #- use fiducial cosmology to get luminosity distance
+            dl=desisim.cosmology.fiducial_cosmology.luminosity_distance(z)
+            
+            #- ratio of luminosity distance between redshift z and 10pc
+            dl_ratio=(10*astropy.units.pc)/desisim.cosmology.fiducial_cosmology.luminosity_distance(z)
+            
+            #- factor 1/(1+z) because energy density per unit observed wavelength
+            scale=dl_ratio**2/(1.+z)
+
+            # just if someone wonders,
+            # templates are SEDs at 10pc : phi_0 = ergs/s/cm2/A at d=10pc , z=0
+            # luminosity is L (ergs/s/A) = (4*pi*(10pc)^2) * phi_0
+            # dd=distance from source , dl=(1+z)*dd=luminosity distance
+            # number of photons per unit area (of energy,time interval,wave interval E_e,dt_e,dwave_e at emission)
+            # dn_photons = L/(4*pi*(dd(z))^2)/E_e*dt_e*dwave_e
+            #            = L/(4*pi*(dd(z))^2)/E_r*dt_r*dwave_r/(1+z)^3
+            # phi_obs = observed SED in ergs/s/cm2/A :
+            # phi_obs = E_r*dn_photons/(dt_r*dwave_r)
+            #         = L/(4*pi*(dd(z))^2)/(1+z)^3
+            #         = phi_0*(10pc/dd(z))^2/(1+z)^3
+            #         = phi_0*(10pc/(dl(z)/(1+z))^2/(1+z)^3
+            #         = phi_0*(10pc/dl(z))^2/(1+z)
+            
+            
+            #- observer frame wave
+            file_obsframe_wave=(1.+z)*file_restframe_wave
+            
+            #- use interpolation routine that conserves flux
+            spectra[index]=desisim.interpolation.general_interpolate_flux_density(wave,file_obsframe_wave,scale*restframe_spectrum)
+            
+    
+    return spectra
