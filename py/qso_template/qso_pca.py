0b1357306a682fa3963e50ed3e24092103bf2c8b
J. Xavier Prochaska <xavier@dhcp-22-101.ucolick.org>
2014-12-01 19:58:15 -0800

    jxp: first pca code

--- py/qso_template/qso_pca.py
+++ py/qso_template/qso_pca.py
@@ -16,110 +16,112 @@ import numpy as np
 import os
 
 from astropy.io import fits
-from xastropy.xutils import xdebug as xdb
+flg_xdb = True
+try: 
+    from xastropy.xutils import xdebug as xdb
+except ImportError:
+    flg_xdb = False
 
-"""
-## Tinkering about
-#   JXP on 01 Dec 2014
+#
+def read_qso_eigen(eigen_fil=None):
+    '''
+    Input the QSO Eigenspectra
+    '''
+    # File
+    if eigen_fil is None:
+        eigen_fil = os.environ.get('IDLSPEC2D_DIR')+'/templates/spEigenQSO-55732.fits'
+    print('Using these eigen spectra: {:s}'.format(eigen_fil))
+    hdu = fits.open(eigen_fil)
+    eigen = hdu[0].data
+    head = hdu[0].header
+    # Rest wavelength
+    eigen_wave = 10.**(head['COEFF0'] + np.arange(head['NAXIS1'])*head['COEFF1'])
 
-hdu = fits.open('spEigenQSO-55732.fits')
-eigen = hdu[0].data  # There are 4 eigenvectors
-wav = 10.**(2.6534 + np.arange(13637)*0.0001)  # Rest-frame, Ang
-xdb.xplot(wav,eigen[0,:])
-  # Looks sensible enough
+    # Return
+    return eigen, eigen_wave
 
-# QSO sample used to generate the PCA eigenvectors
-qsos = hdu[1].data
-xdb.xhist(qsos['REDSHIFT']) # Skewed to high z
-"""
+##
+def fit_eigen(flux,ivar,eigen_flux):
+    '''
+    Fit the spectrum with the eigenvectors.
+    Pass back the coefficients
+    '''
+    #C = np.diag(1./ivar)
+    Cinv = np.diag(ivar)
+    A = eigen_flux.T
 
+    #alpha = np.dot(A.T, np.linalg.solve(C, A))  # Numerical Recipe notation
+    alpha = np.dot(A.T, np.dot(Cinv,A))
+    cov = np.linalg.inv(alpha)
+    #beta = np.dot(A.T, np.linalg.solve(C, y))
+    beta = np.dot(A.T, np.dot(Cinv, flux))
+    acoeff = np.dot(cov, beta)
 
-"""
-Let's test in IDL on a BOSS spectrum
+    # Return
+    return acoeff
 
-cd /Users/xavier/BOSS/DR10/BOSSLyaDR10_spectra_v2.1/5870
+##
+def do_boss_lya(debug=False):
+    '''
+    Generate PCA coeff for the BOSS Lya DR10 dataset, v2.1
+    '''
+    # Eigen
+    eigen, eigen_wave = read_qso_eigen()
 
-IDL> boss_objinf, [5870,1000], /plot
-    Name             RA(2000)   DEC(2000) Mag(R) zobj
-5870-56065-100     14:07:37.28 +21:48:38.5 21.06 2.180
+    # Open the BOSS catalog file
+    boss_cat_fil = os.environ.get('BOSSPATH')+'/DR10/BOSSLyaDR10_cat_v2.1.fits.gz'
+    bcat_hdu = fits.open(boss_cat_fil)
+    t_boss = bcat_hdu[1].data
+    nqso = len(t_boss)
 
-Looks like a fine quasar
+    pca_val = np.zeros((nqso, 4))
 
-Read the eigenspectra
+    # Loop us -- Should spawn on multiple CPU
+    #for ii in range(nqso):
+    datdir =  os.environ.get('BOSSPATH')+'/DR10/BOSSLyaDR10_spectra_v2.1/'
+    for ii in range(1000):
+        if (ii % 100) == 0:
+            print('ii = {:d}'.format(ii))
+        # Spectrum file
+        pnm = str(t_boss['PLATE'][ii])
+        fnm = str(t_boss['FIBERID'][ii]).rjust(4,str('0'))
+        mjd = str(t_boss['MJD'][ii])
+        sfil = datdir+pnm+'/speclya-'
+        sfil = sfil+pnm+'-'+mjd+'-'+fnm+'.fits.gz'
+        # Read spectrum
+        spec_hdu = fits.open(sfil)
+        t = spec_hdu[1].data
+        flux = t['flux']
+        wave = 10.**t['loglam']
+        ivar = t['ivar']
+        zqso = t_boss['z_pipe'][ii]
 
-eigen = xmrdfits(getenv('IDLSPEC2D_DIR')+'/templates/spEigenQSO-55732.fits',0,head)
-eigen_wave = 10.^(sxpar(head,'COEFF0') + dindgen(sxpar(head,'NAXIS1'))*sxpar(head,'COEFF1'))
+        wrest  = wave / (1+zqso)
+        npix = len(wrest)
 
+        # Pack
+        imn = np.argmin(np.abs(wrest[0]-eigen_wave))
+        imx = npix+imn
+        eigen_flux = eigen[:,imn:imx]
 
-BOSS spectrum
+        # FIT
+        acoeff = fit_eigen(flux, ivar, eigen_flux)
+        pca_val[ii,:] = acoeff
 
-boss_readspec, 'speclya-5870-56065-1000.fits', flux, wave, ivar=ivar
-zqso = 2.180
-wrest  = wave / (1+zqso)
-npix = n_elements(wrest)
+        # Check
+        if debug is True:
+            model = np.dot(eigen_flux.T,acoeff)
+            if flg_xdb is True:
+                xdb.xplot(wrest, flux, model)
+            xdb.set_trace()
 
-Are they 'registered'?
-mn = min(abs(wrest[0]-eigen_wave),imn)
-x_splot, findgen(npix), flux, ytwo=eigen[imn:*,0]/5292*4.4
-  Yes, they are..
+    xdb.set_trace()
+    
+## #################################    
+## #################################    
+## TESTING
+## #################################    
+if __name__ == '__main__':
+    # Run
+    do_boss_lya()#debug=True)
 
-Build the inputs to computechi2
-
-objflux = flux
-sqivar = ivar
-imx = npix+imn-1
-starflux = eigen[imn:imx,*]
-
-chi2 = computechi2(objflux, sqivar, starflux, acoeff=acoeff, yfit=yfit)
-  ;; The fit looks ok
-
-IDL> print, acoeff
-   0.00045629701  -0.00034376233   0.00047730298   0.00045008259
-"""
-
-
-# Read Eigenspectrum
-
-hdu = fits.open(os.environ.get('IDLSPEC2D_DIR')+'/templates/spEigenQSO-55732.fits')
-eigen = hdu[0].data
-head = hdu[0].header
-eigen_wave = 10.**(head['COEFF0'] + np.arange(head['NAXIS1'])*head['COEFF1'])
-
-#xdb.xplot(eigen_wave, eigen[0,:])
-#xdb.set_trace()
-
-#BOSS spectrum
-
-boss_hdu = fits.open('/Users/xavier/BOSS/DR10/BOSSLyaDR10_spectra_v2.1/5870/speclya-5870-56065-1000.fits.gz')
-t = boss_hdu[1].data
-flux = t['flux']
-wave = 10.**t['loglam']
-ivar = t['ivar']
-
-zqso = 2.180
-wrest  = wave / (1+zqso)
-npix = len(wrest)
-imx = npix+imn
-
-imn = np.argmin(np.abs(wrest[0]-eigen_wave))
-eigen_flux = eigen[:,imn:imx]
-
-
-# Generate the matrices
-"""
-Now Python with simple approach following Dan F-M http://dan.iel.fm/emcee/current/user/line/
-"""
-
-C = np.diag(1./ivar)
-A = eigen_flux.T
-y = flux
-
-cov = np.linalg.inv(np.dot(A.T, np.linalg.solve(C, A)))
-acoeff = np.dot(cov, np.dot(A.T, np.linalg.solve(C, y)))
-
-#In [48]: acoeff
-#Out[48]: array([ 0.00045461, -0.00012764,  0.00010843,  0.0003281 ], dtype=float32)
-
-model = np.dot(A,acoeff)
-xdb.xplot(wrest, flux, model)
-  # Looks good to me
