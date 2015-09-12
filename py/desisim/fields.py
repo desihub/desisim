630c1a0e9f7ddea954a47081d544f319f2f087c6
Jaime Forero <j.e.forero.romero@gmail.com>
2015-09-12 11:56:10 -0500

    functiosn for working on simulated nights of observation. Finds available fields. Determines bright/dark time

--- py/desisim/fields.py
+++ py/desisim/fields.py
@@ -2,8 +2,10 @@
 Functions for working on simulated nights of observation. 
 """
 
+
 import ephem as eph
 import numpy as np
+import desimodel.io
 
 _observer = None
 
@@ -61,3 +63,38 @@ def separation_azimuth_moon():
     moon.compute(_observer.date)
     a = eph.separation([up_ra,up_dec], [moon.ra, moon.dec])
     return a
+
+def points_inside_azimuth_cone(ra_list, dec_list, id_list, angle_to_zenith):
+    """
+    Selects a subset of points close to the zenith
+    
+    Args:
+        ra_list (float): np.array with RA angles in degrees.
+        dec_list (float): np.array with Dec angles in degrees.
+        id_list (int): np.array with integers indexing the RA, DEC values
+        angle_to_zenith (float): angle in degrees to zenith below which the list points are selected.
+    Returns:
+        The subset of points is represented by three arrays.
+        ra_sublist (float): np.array with RA angles in degrees.
+        dec_sublist (float): np.array with Dec angles in degrees.
+        id_sublist (int): np.array with integers indexing the RA, DEC values.
+    """
+    assert angle_to_zenith >= 0.0, "Angle to zenith must be positive"
+    assert angle_to_zenith <= 360.0, "Angle to zenith must be less than 360 degrees"
+    
+    ra_sublist = np.empty((0))
+    dec_sublist = np.empty((0))
+    id_sublist = np.empty((0), dtype='int')
+    n_tiles = np.size(id_list)
+
+    ra_list_radians = ra_list * np.pi/180.0
+    dec_list_radians = dec_list * np.pi/180.0
+    
+    ra_up, dec_up = radec_zenith()
+    for i_tile in np.arange(n_tiles):
+        a = eph.separation([ra_up,dec_up], [ra_list_radians[i_tile], dec_list_radians[i_tile]])
+        if(float(a) < (angle_to_zenith*np.pi/180.0)):
+            id_sublist = np.append(id_sublist, id_list[i_tile])
+            ra_sublist = np.append(ra_sublist, ra_list[i_tile])
+            dec_sublist = np.append(dec_sublist, dec_list[i_tile])
+    return ra_sublist, dec_sublist, id_sublist
