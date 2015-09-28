64eaa5b806f671aebca3b6bf821cfbed89bab380
Stephen Bailey <stephen.code@gmail.com>
2015-09-28 16:51:01 -0700

    enable 'python setup.py test'; improve versioning

--- py/desisim/__init__.py
+++ py/desisim/__init__.py
@@ -12,24 +12,18 @@ from . import obs
 from . import io
 from . import targets
 
-__version__ = 'unknown'
-def version():
-    global __version__
-    if __version__ != 'unknown':
+__version__ = '0.4.1.dev'
+def gitversion():
+    from subprocess import Popen, PIPE
+    try:
+        p = Popen(['git', "describe", "--tags", "--dirty", "--always"], stdout=PIPE)
+    except EnvironmentError:
         return __version__
-    else:
-        from subprocess import Popen, PIPE
-        try:
-            p = Popen(['git', "describe", "--tags", "--dirty", "--always"], stdout=PIPE)
-        except EnvironmentError:
-            return __version__
-        out = p.communicate()[0]
-        if p.returncode != 0:
-            return __version__
-            
-        __version__ = out.rstrip()
+    out = p.communicate()[0]
+    if p.returncode != 0:
         return __version__
+        
+    __version__ = out.rstrip()
+    return __version__
 
-#- This requires making a system call every time desisim is loaded.  Yuck.
-# __version__ = version()
         
