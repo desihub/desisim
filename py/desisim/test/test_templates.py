08238b7b9536ea03132eecb7514904622078d89e
moustakas <john.moustakas@gmail.com>
2016-04-29 14:16:58 -0400

    add tests for add_SNeIa option in ELG, LRG, and BGS templates

--- py/desisim/test/test_templates.py
+++ py/desisim/test/test_templates.py
@@ -3,7 +3,7 @@ from __future__ import division
 import os
 import unittest
 import numpy as np
-from desisim.templates import ELG, LRG, QSO, STAR, FSTD, MWS_STAR
+from desisim.templates import ELG, LRG, QSO, STAR, FSTD, MWS_STAR, BGS
 
 desimodel_data_available = 'DESIMODEL' in os.environ
 desi_templates_available = 'DESI_ROOT' in os.environ
@@ -97,6 +97,17 @@ class TestTemplates(unittest.TestCase):
                 if ((T != QSO) and (T != STAR)) or ((col != 'W1MAG') and (col != 'W2MAG')):
                     self.assertTrue(np.all(meta1[col] == meta2[col]),
                         'metadata {} inconsistent for objtype {}'.format(col, Tx.objtype))
+
+    @unittest.skipUnless(desi_basis_templates_available, '$DESI_BASIS_TEMPLATES was not detected.')
+    def test_sne(self):
+        '''Test options for adding in SNeIa spectra'''
+        for T in [ELG, LRG, BGS]:
+            template_factory = T(wave=self.wave, add_SNeIa=True)
+            flux, wave, meta = template_factory.make_templates(self.nspec,sne_rfluxratiorange=(0.5,0.7))
+            self._check_output_size(flux, wave, meta)
+            self.assertTrue('SNE_TEMPLATEID' in meta.dtype.names)
+            self.assertTrue('SNE_RFLUXRATIO' in meta.dtype.names)
+            self.assertTrue('SNE_EPOCH' in meta.dtype.names)
 
     @unittest.expectedFailure
     def test_missing_wise_mags(self):
