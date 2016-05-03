fe13ba8495f7c1d45c3e32ee4165478decc49a08
moustakas <john.moustakas@gmail.com>
2016-05-02 21:22:08 -0400

    debugging the test_sne function

--- py/desisim/test/test_templates.py
+++ py/desisim/test/test_templates.py
@@ -115,9 +115,11 @@ class TestTemplates(unittest.TestCase):
     @unittest.skipUnless(desi_basis_templates_available, '$DESI_BASIS_TEMPLATES was not detected.')
     def test_sne(self):
         '''Test options for adding in SNeIa spectra'''
-        for T in [ELG, LRG, BGS]:
+        for T in [BGS]:
+        #for T in [ELG, LRG, BGS]:
             template_factory = T(wave=self.wave, add_SNeIa=True)
-            flux, wave, meta = template_factory.make_templates(self.nspec,sne_rfluxratiorange=(0.5,0.7))
+            flux, wave, meta = template_factory.make_templates(self.nspec, sne_rfluxratiorange=(0.5,0.7))
+            #import pdb ; pdb.set_trace()
             self._check_output_size(flux, wave, meta)
             self.assertTrue('SNE_TEMPLATEID' in meta.dtype.names)
             self.assertTrue('SNE_RFLUXRATIO' in meta.dtype.names)
