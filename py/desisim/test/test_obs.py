bbf27b2e9bcc441159df54f731184294acf2af8f
moustakas <john.moustakas@gmail.com>
2016-08-18 00:01:15 -0400

    remove leftover trace

--- py/desisim/test/test_obs.py
+++ py/desisim/test/test_obs.py
@@ -79,8 +79,6 @@ class TestObs(unittest.TestCase):
                     self.assertTrue(maxsky > 1, 'suspiciously low {} sky flux ({}); wrong units?'.format(objtype, maxsky))
                     self.assertTrue(maxsky < 1e5, 'suspiciously high {} sky flux ({}); wrong units?'.format(objtype, maxsky))
                     if objtype != 'SKY':
-                        if maxflux <= 0.1:
-                            import pdb ; pdb.set_trace()
                         self.assertTrue(maxflux > 0.1, 'suspiciously low {} flux ({}); wrong units?'.format(objtype, maxflux))
                         self.assertTrue(maxflux < 1e5, 'suspiciously high {} flux ({}); wrong units?'.format(objtype, maxflux))
                     else:
