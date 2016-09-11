fa9fab54a412a466ac7f5d97b65cd61657f55324
Stephen Bailey <stephen.code@gmail.com>
2016-09-10 18:36:11 -0700

    update .travis.yml and changes.rst prior to py3 PR

--- py/desisim/io.py
+++ py/desisim/io.py
@@ -515,7 +515,8 @@ def read_cosmics(filename, expid=1, shape=None, jitter=True):
     iixy = np.s_[ny:2*ny, nx:2*nx]
     mean, median, std = sigma_clipped_stats(pix[iixy], sigma=3, iters=5)
     meta['RDNOISE4'] = std
-
+    fx.close()
+    
     return Image(pix, ivar, mask, meta=meta)
 
 #-------------------------------------------------------------------------
