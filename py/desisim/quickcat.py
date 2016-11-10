a2659a266840e85d7a7b76bc6cf02653f0c85cd5
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-11-10 09:05:49 -0800

    unified interface for get_redshift and get_redshift_efficiency with/without obsconditions inputs

--- py/desisim/quickcat.py
+++ py/desisim/quickcat.py
@@ -23,6 +23,8 @@ import astropy.constants
 c = astropy.constants.c.to('km/s').value
 
 from desitarget.targets import desi_mask
+from desitarget.targets import bgs_mask
+from desitarget.targets import mws_mask
 
 #- redshift errors and zwarn fractions from DESI-1657
 #- sigmav = c sigmaz / (1+z)
@@ -44,30 +46,15 @@ _zwarn_fraction = {
     'UNKNOWN': 1.0,
 }
 
-def get_redshift_efficiency_zmag(truetype, targetid, truez, gmag, targets_in_tile):
+def get_redshift_efficiency(truetype, truez, targetid, targets_in_tile, obsconditions=None, gmag=None):
     """
-    Simple model to get the redshift effiency from the true redshift and observed g magnidutes.
-    Args:
-        truetype : string, could be 'ELG', 'LRG', 'QSO', 'STAR', 'SKY', or 'UNKNOWN'
-        targetid : array of targetids.
-        truez: array of true redshift values.
-        gmag: array of g magnitudes.
-        targets_in_tile: dictionary. Keys correspond to tileids, its values are the 
-            arrays of targetids observed in that tile.
-    Outputs:
-       p: array marking the probability to get this redshift right. This must have the size of targetid.
-    """
-    n = len(targetid)
-    p = np.ones(n)
-    return p
-
-def get_redshift_efficiency_obsconditions(truetype, targetid, tiles_for_target, obsconditions):
-    """
-    Simple model to get the redshift effiency from the observational conditions.
+    Simple model to get the redshift effiency from the observational conditions or observed magnitudes+redshuft
     Args:
        truetype: string, could be 'ELG', 'LRG', 'QSO', 'STAR', 'SKY', or 'UNKNOWN'
-       tilefiles: list of fiberassign tile files that were observed.
-       tiles_for_targets: dictionary. Keys correspond to targetids observed, its values are the tileid where it was observed.
+       truez: array of true redshift values.
+       targets_in_tile: dictionary. Keys correspond to tileids, its values are the 
+            arrays of targetids observed in that tile.
+
        obsconditions: Dictionary with the observational conditions for every tile.
            It inclues at least the following keys
            'TILEID': array of tile IDs
@@ -77,90 +64,108 @@ def get_redshift_efficiency_obsconditions(truetype, targetid, tiles_for_target, 
            'MOONFRAC': array of moonfraction values on a tile 
            'SEEING': array of seeing values on a tile
            
+       gmag: array of observed magnitudes to diagnose redshift efficiency.
     Outputs:
     p: array marking the probability to get this redshift right. This must have the size of targetid.
-     """
-
+    """
 
     n = len(targetid)
     p = np.ones(n)
-    we_have_a_model = True    
-    
-    p_v = [1.0, 0.0, 0.0]
-    p_w = [1.0, 0.0, 0.0]
-    p_x = [1.0, 0.0, 0.0]
-    p_y = [1.0, 0.0, 0.0]
-    p_z = [1.0, 0.0, 0.0]
-    sigma_r = 0.0
-    p_total = 1.0
-    if(truetype=='LRG'):
-        p_v = [1.0, 0.15, 0.5]
-        p_w = [1.0, 0.4, 0.0]
-        p_x = [1.0, 0.06, 0.05]
-        p_y = [1.0, 0.0, 0.08]
+
+    # reverse the dictionary targets in tile
+    tiles_for_target = reverse_dictionary(targets_in_tile)
+    for i in range(n):
+        t_id = targetid[i]
+        if t_id not in tiles_for_target.keys():
+            p[i] = 0.0
+
+    if (obsconditions is not None) and (gmag is None): # use this if we only have obsconditions        
+        we_have_a_model = True    
+        
+        p_v = [1.0, 0.0, 0.0]
+        p_w = [1.0, 0.0, 0.0]
+        p_x = [1.0, 0.0, 0.0]
+        p_y = [1.0, 0.0, 0.0]
         p_z = [1.0, 0.0, 0.0]
-        sigma_r = 0.02
-        p_total = 0.95
-    elif(truetype=='QSO'):
-        p_v = [1.0, -0.2, 0.3]
-        p_w = [1.0, -0.5, 0.6]
-        p_x = [1.0, -0.1, -0.075]
-        p_y = [1.0, -0.08, -0.04]
-        p_z = [1.0, 0.0, 0.0]
-        sigma_r = 0.05
-        p_total = 0.90
-    elif(truetype=='ELG'):
-        p_v = [1.0, -0.1, -0.2]
-        p_w = [1.0, 0.25, -0.75]
-        p_x = [1.0, 0.0, 0.05]
-        p_y = [1.0, 0.2, 0.1]
-        p_z = [1.0, -10.0, 300.0]
-        sigma_r = 0.02
-        p_total = 0.95
-    else:
-        print('WARNING. We are not modelling yet the z efficiency for type: {}. Set to 1.0'.format(truetype))
-        we_have_a_model = False
+        sigma_r = 0.0
+        p_total = 1.0
+        if(truetype=='LRG'):
+            p_v = [1.0, 0.15, 0.5]
+            p_w = [1.0, 0.4, 0.0]
+            p_x = [1.0, 0.06, 0.05]
+            p_y = [1.0, 0.0, 0.08]
+            p_z = [1.0, 0.0, 0.0]
+            sigma_r = 0.02
+            p_total = 0.95
+        elif(truetype=='QSO'):
+            p_v = [1.0, -0.2, 0.3]
+            p_w = [1.0, -0.5, 0.6]
+            p_x = [1.0, -0.1, -0.075]
+            p_y = [1.0, -0.08, -0.04]
+            p_z = [1.0, 0.0, 0.0]
+            sigma_r = 0.05
+            p_total = 0.90
+        elif(truetype=='ELG'):
+            p_v = [1.0, -0.1, -0.2]
+            p_w = [1.0, 0.25, -0.75]
+            p_x = [1.0, 0.0, 0.05]
+            p_y = [1.0, 0.2, 0.1]
+            p_z = [1.0, -10.0, 300.0]
+            sigma_r = 0.02
+            p_total = 0.95
+        else:
+            print('WARNING in desisim.quickcat.get_redshift_efficiency()')
+            print('\t We are not modelling yet the redshift efficiency for type: {}. Set to 1.0'.format(truetype))
+            we_have_a_model = False
 
 
-    if we_have_a_model:
-        normal_values = np.random.normal(size=len(obsconditions['TILEID'])) # set of normal values across tiles
-        for i in range(n):
-            t_id = targetid[i]
-            if t_id not in tiles_for_target.keys():
-                p[i] = 0.0
-            else:
-                tiles = tiles_for_target[t_id]
-                p_final = 0.0 # just in case a target is found in multiple tiles
-                for tileid in tiles:            
-                    ii = (obsconditions['TILEID'] == tileid)
+            if we_have_a_model:
+                normal_values = np.random.normal(size=len(obsconditions['TILEID'])) # set of normal values across tiles
+                for i in range(n):
+                    t_id = targetid[i]
+                    if t_id in tiles_for_target.keys():
+                        tiles = tiles_for_target[t_id]
+                        p_final = 0.0 # just in case a target is found in multiple tiles
+                        for tileid in tiles:            
+                            ii = (obsconditions['TILEID'] == tileid)
             
-                    v = obsconditions['AIRMASS'][ii] - np.mean(obsconditions['AIRMASS'])
-                    pv  = p_v[0] + p_v[1] * v + p_v[2] * (v**2 - np.mean(obsconditions['AIRMASS']**2))
-                    
-                    w = obsconditions['EBMV'][ii] - np.mean(obsconditions['EBMV'])
-                    pw = p_w[0] + p_w[1] * w + p_w[2] * (w**2 - np.mean(obsconditions['EBMV']**2))
-                    
-                    x = obsconditions['SEEING'][ii] - np.mean(obsconditions['SEEING'])
-                    px = p_x[0] + p_x[1]*x + p_x[2] * (x**2 - np.mean(obsconditions['SEEING']**2))
-                    
-                    y = obsconditions['LINTRANS'][ii] - np.mean(obsconditions['LINTRANS'])
-                    py = p_y[0] + p_y[1]*y + p_y[2] * (y**2 - np.mean(obsconditions['LINTRANS']**2))
-                    
-                    z = obsconditions['MOONFRAC'][ii] - np.mean(obsconditions['MOONFRAC'])
-                    pz = p_z[0] + p_z[1]*z + p_z[2] * (z**2 - np.mean(obsconditions['MOONFRAC']**2))
-                    
-                    pr = 1.0 + sigma_r * normal_values[ii]
-                    
-                    p[i] = p_total * pv * pw * px * py * pz * pr 
-                
-                    if(p[i]>1.0):
-                        p[i] = 1.0
-                        
-                    if(p_final > p[i]): #select the best condition of all tiles
-                        p[i] = p_final
-                        p_final = p[i]        
-    return p
+                            v = obsconditions['AIRMASS'][ii] - np.mean(obsconditions['AIRMASS'])
+                            pv  = p_v[0] + p_v[1] * v + p_v[2] * (v**2 - np.mean(obsconditions['AIRMASS']**2))
+                            
+                            w = obsconditions['EBMV'][ii] - np.mean(obsconditions['EBMV'])
+                            pw = p_w[0] + p_w[1] * w + p_w[2] * (w**2 - np.mean(obsconditions['EBMV']**2))
+                            
+                            x = obsconditions['SEEING'][ii] - np.mean(obsconditions['SEEING'])
+                            px = p_x[0] + p_x[1]*x + p_x[2] * (x**2 - np.mean(obsconditions['SEEING']**2))
+    
+                            y = obsconditions['LINTRANS'][ii] - np.mean(obsconditions['LINTRANS'])
+                            py = p_y[0] + p_y[1]*y + p_y[2] * (y**2 - np.mean(obsconditions['LINTRANS']**2))
+                            
+                            z = obsconditions['MOONFRAC'][ii] - np.mean(obsconditions['MOONFRAC'])
+                            pz = p_z[0] + p_z[1]*z + p_z[2] * (z**2 - np.mean(obsconditions['MOONFRAC']**2))
+                            
+                            pr = 1.0 + sigma_r * normal_values[ii]
 
+                            p[i] = p_total * pv * pw * px * py * pz * pr 
+                            
+                            if(p[i]>1.0):
+                                p[i] = 1.0
+                        
+                            if(p_final > p[i]): #select the best condition of all tiles
+                                p[i] = p_final
+                                p_final = p[i]        
+        
+    elif (obsconditions is None) and (gmag is not None): # use this if we only have gmag
+        we_have_a_model = True    
+
+    elif (obsconditions is None) and (gmag is None): # use this if we don't have any input
+        we_have_a_model = True    
+    else:
+        print('ERROR in desisim.quickcat.get_redshift_efficiency()')
+        print('\t Contradictory inputs.')
+        sys.exit()
+
+    return p
 
 def reverse_dictionary(a):
     """
@@ -186,7 +191,7 @@ def reverse_dictionary(a):
                 b[k].append(i[0])            
     return b
 
-def get_observed_redshifts_obsconditions(truetype, truez, targetid, targets_in_tile, obsconditions, gmag=None):
+def get_observed_redshifts(truetype, truez, targetid, targets_in_tile, obsconditions=None, gmag=None):
     """
     Returns observed z, zerr, zwarn arrays given true object types and redshifts
 
@@ -203,7 +208,7 @@ def get_observed_redshifts_obsconditions(truetype, truez, targetid, targets_in_t
             'LINTRANS': array of transmission values on a tile
             'MOONFRAC': array of moonfraction values on a tile 
             'SEEING': array of seeing values on a tile
-        gmag: array of g magnitudes 
+       gmag: array of observed magnitudes to diagnose redshift efficiency.
            
     Returns tuple of (zout, zerr, zwarn)
 
@@ -217,62 +222,44 @@ def get_observed_redshifts_obsconditions(truetype, truez, targetid, targets_in_t
     n_tiles = len(obsconditions['TILEID'])
     
     if(n_tiles!=len(targets_in_tile.keys())):
-        print('Number of obsconditions different from targets_in_tile.')
+        print('ERROR desisim.quickcat.get_observed_redshifts()')
+        print('\t Number of obsconditions different from targets_in_tile.')
         sys.exit()
 
-    # reverse the dictionary targets in tile
-    tiles_for_targets = reverse_dictionary(targets_in_tile)
 
     for objtype in objtypes:
-        ii = (truetype == objtype)
-        p_obs = get_redshift_efficiency_obsconditions(objtype, targetid[ii], tiles_for_targets, obsconditions)            
-        z_eff = p_obs.copy()
-        
-        if gmag is not None:
-            p_eff = get_redshift_efficiency_zmag(truetype[ii], targetid[ii], truez[ii], gmag[ii], targets_in_tile)
-            z_eff *= p_eff
-            
-        n = np.count_nonzero(ii)
-        
-        r = np.random.random(n)
-        jj = r > z_eff
-        zwarn_type = np.zeros(n, dtype=np.int32)
-        zwarn_type[jj] = 4
+        if objtype in _sigma_v.keys():
+            ii = (truetype == objtype)
+            n = np.count_nonzero(ii)        
+            zerr[ii] = _sigma_v[objtype] * (1+truez[ii]) / c
+            zout[ii] += np.random.normal(scale=zerr[ii])
 
-        zerr[ii] = _sigma_v[objtype] * (1+truez[ii]) / c
-        zout[ii] += np.random.normal(scale=zerr[ii])
-        zwarn[ii] = zwarn_type
+            if (obsconditions is not None) or (gmag is not None):
+                p_obs = get_redshift_efficiency(objtype, truez[ii], targetid[ii], targets_in_tile, obsconditions=obsconditions, gmag=gmag)            
+                z_eff = p_obs.copy()            
+                r = np.random.random(n)
+                jj = r > z_eff
+                zwarn_type = np.zeros(n, dtype=np.int32)
+                zwarn_type[jj] = 4
+                zwarn[ii] = zwarn_type
+
+            elif(obsconditions is None) and (gmag is None):
+                #- randomly select some objects to set zwarn
+                num_zwarn = int(_zwarn_fraction[objtype] * n)
+                if num_zwarn > 0:
+                    jj = np.random.choice(np.where(ii)[0], size=num_zwarn, replace=False)
+                    zwarn[jj] = 4
+            else:
+                print('ERROR desisim.quickcat.get_observed_redshifts()')
+                print('\t Contradicting inputs')
+                sys.exit()
+        else:
+            print('WARNING desisim.quickcat.get_observed_redshifts()')
+            print('\t We dont have a model for objtype {}. Simply assigning a redshift from the truth table.'.format(objtype))
+            
             
     return zout, zerr, zwarn
 
-
-def get_observed_redshifts(truetype, truez):
-    """
-    Returns observed z, zerr, zwarn arrays given true object types and redshifts
-
-    Args:       
-        truetype : array of ELG, LRG, QSO, STAR, SKY, or UNKNOWN
-        truez: array of true redshifts
-
-    Returns tuple of (zout, zerr, zwarn)
-
-    TODO: Add BGS, MWS support
-    """
-    zout = truez.copy()
-    zerr = np.zeros(len(truez), dtype=np.float32)
-    zwarn = np.zeros(len(truez), dtype=np.int32)
-    for objtype in _sigma_v:
-        ii = (truetype == objtype)
-        n = np.count_nonzero(ii)
-        zerr[ii] = _sigma_v[objtype] * (1+truez[ii]) / c
-        zout[ii] += np.random.normal(scale=zerr[ii])
-        #- randomly select some objects to set zwarn
-        num_zwarn = int(_zwarn_fraction[objtype] * n)
-        if num_zwarn > 0:
-            jj = np.random.choice(np.where(ii)[0], size=num_zwarn, replace=False)
-            zwarn[jj] = 4
-
-    return zout, zerr, zwarn
 
 def get_obsconditions(tilefiles):
     """
@@ -345,7 +332,6 @@ def quickcat(tilefiles, targets, truth, zcat=None, perfect=False):
         nobs.update(fibassign['TARGETID'][ii])
         targets_in_tile[tile_id] = fibassign['TARGETID'][ii]
 
-
     #- Count how many times each target was observed in previous zcatalog
     #- NOTE: assumes that no tiles have been repeated
     if zcat is not None:
@@ -382,15 +368,21 @@ def quickcat(tilefiles, targets, truth, zcat=None, perfect=False):
     #- Add ZERR and ZWARN
     ### print('Adding ZERR and ZWARN')
     if not perfect:
-        #- GALAXY -> ELG or LRG
+        #- GALAXY -> ELG, LRG, BGS
         objtype = newzcat['SPECTYPE'].copy()
         isLRG = (objtype == 'GALAXY') & ((targets['DESI_TARGET'] & desi_mask.LRG) != 0)
         isELG = (objtype == 'GALAXY') & ((targets['DESI_TARGET'] & desi_mask.ELG) != 0)
+        isBGS = (objtype == 'GALAXY') & ((targets['DESI_TARGET'] & bgs_mask.BGS_BRIGHT) != 0)
+        isBGS &= (objtype == 'GALAXY') & ((targets['DESI_TARGET'] & bgs_mask.BGS_FAINT) != 0)
         objtype[isLRG] = 'LRG'
         objtype[isELG] = 'ELG'
+        objtype[isBGS] = 'BGS'
 
+        #get the observational conditions for the current tilefiles
         obsconditions = get_obsconditions(tilefiles)
-        z, zerr, zwarn = get_observed_redshifts_obsconditions(objtype, newzcat['Z'], newzcat['TARGETID'], targets_in_tile, obsconditions)
+
+        #get the redshifts
+        z, zerr, zwarn = get_observed_redshifts(objtype, newzcat['Z'], newzcat['TARGETID'], targets_in_tile, obsconditions)
 
         newzcat['Z'] = z  #- update with noisy redshift
     else:
