339bbf5b91491a59225c6f6268604b66650e4ad4
John Moustakas <john.moustakas@gmail.com>
2017-02-26 08:58:41 -0500

    update tests

--- py/desisim/lya_spectra.py
+++ py/desisim/lya_spectra.py
@@ -7,27 +7,33 @@ Function to simulate a QSO spectrum including Lyman-alpha absorption.
 
 from __future__ import division, print_function
 
-def get_spectra(lyafile, templateid=None, wave=None, normfilter='sdss2010-g',
-                rand=None, qso=None):
+def get_spectra(lyafile, nqso=None, wave=None, templateid=None, normfilter='sdss2010-g',
+                seed=None, rand=None, qso=None):
     '''Generate a QSO spectrum which includes Lyman-alpha absorption.
 
     Args:
         lyafile (str): name of the Lyman-alpha spectrum file to read.
-        templateid (int numpy.ndarray, optional): indices of the spectra
-          (0-indexed) to read from LYAFILE (default is to read everything).
+        nqso (int, optional): number of spectra to generate (starting from the
+          first spectrum; if more flexibility is needed use TEMPLATEID).
         wave (numpy.ndarray, optional): desired output wavelength vector.
+        templateid (int numpy.ndarray, optional): indices of the spectra
+          (0-indexed) to read from LYAFILE (default is to read everything).  If
+          provided together with NQSO, TEMPLATEID wins.
         normfilter (str, optional): normalization filter
+        seed (int, optional): Seed for random number generator.
         rand (numpy.RandomState, optional): RandomState object used for the
-          random number generation.
+          random number generation.  If provided together with SEED, this
+          optional input superseeds the numpy.RandomState object instantiated by
+          SEED.
         qso (desisim.templates.QSO, optional): object with which to generate
           individual spectra/templates.
 
     Returns:
-      flux (numpy.ndarray): Array [nmodel, npix] of observed-frame spectra
-        (erg/s/cm2/A).
-      wave (numpy.ndarray): Observed-frame [npix] wavelength array (Angstrom).
-      meta (astropy.Table): Table of meta-data [nmodel] for each output spectrum
-        with columns defined in desisim.io.empty_metatable *plus* RA, DEC.
+        flux (numpy.ndarray): Array [nmodel, npix] of observed-frame spectra
+          (erg/s/cm2/A).
+        wave (numpy.ndarray): Observed-frame [npix] wavelength array (Angstrom).
+        meta (astropy.Table): Table of meta-data [nmodel] for each output spectrum
+          with columns defined in desisim.io.empty_metatable *plus* RA, DEC.
 
     '''
     import numpy as np
@@ -41,17 +47,22 @@ def get_spectra(lyafile, templateid=None, wave=None, normfilter='sdss2010-g',
 
     h = fitsio.FITS(lyafile)
     if templateid is None:
-        nqso = len(h)-1
+        if nqso is None:
+            nqso = len(h)-1
+        templateid = np.arange(nqso)
     else:
+        templateid = np.array(templateid)
         nqso = len(templateid)
+    print(templateid)
 
     if rand is None:
-        rand = np.random.RandomState()
-    seed = rand.randint(2**32, size=nqso)
+        rand = np.random.RandomState(seed)
+    templateseed = rand.randint(2**32, size=nqso)
 
     #heads = [head.read_header() for head in h[templateid + 1]]
     heads = []
     for indx in templateid:
+        print(indx + 1)
         heads.append(h[indx + 1].read_header())
 
     zqso = np.array([head['ZQSO'] for head in heads])
@@ -72,13 +83,13 @@ def get_spectra(lyafile, templateid=None, wave=None, normfilter='sdss2010-g',
     meta['TEMPLATEID'] = templateid
     meta['REDSHIFT'] = zqso
     meta['MAG'] = mag_g
-    meta['SEED'] = seed
+    meta['SEED'] = templateseed
     meta['RA'] = ra
     meta['DEC'] = dec
     
     for ii, indx in enumerate(templateid):
         flux1, _, meta1 = qso.make_templates(nmodel=1, redshift=np.array([zqso[ii]]),
-                                             mag=np.array([mag_g[ii]]), seed=seed[ii])
+                                             mag=np.array([mag_g[ii]]), seed=templateseed[ii])
 
         # read lambda and forest transmission
         data = h[indx + 1].read()
