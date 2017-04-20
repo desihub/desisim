ceb80ba34bbdbebb52ff1f64e06cd65876f8e131
Stephen Bailey <stephen.code@gmail.com>
2017-04-19 17:45:12 -0700

    newexp-desi working again

---
+++ py/desisim/scripts/newexp.py
@@ -0,0 +1,60 @@
+from __future__ import absolute_import, division, print_function
+import sys, os
+import argparse
+
+import numpy as np
+import astropy.table
+import astropy.time
+
+from desisim.newexp import newexp
+import desisim.io
+
+def parse(options=None):
+    parser=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    #- Required
+    parser.add_argument('--fiberassign', type=str, help="input fiberassign tile file")
+    parser.add_argument('--obslist', type=str, help="input surveysim obslist file")
+    parser.add_argument('--mockdir', type=str, help="directory with mock targets and truth")
+    parser.add_argument('--obsnum', type=int, default=None, help="index in obslist file to use")
+
+    #- Optional
+    parser.add_argument('--expid', type=int, default=None, help="exposure ID")
+    parser.add_argument('--outdir', type=str, help="output directory")
+    parser.add_argument('--nspec', type=int, default=None, help="number of spectra to include")
+
+    if options is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(options)
+
+    if args.expid is None:
+        args.expid = args.obsnum
+
+    return args
+
+def main(args=None):
+    
+    if args is None:
+        args = parse()
+
+    fiberassign = astropy.table.Table.read(args.fiberassign, 'FIBER_ASSIGNMENTS')
+
+    if args.obslist.endswith('.ecsv'):
+        obslist = astropy.table.Table.read(args.obslist, format='ascii.ecsv')
+    else:
+        obslist = astropy.table.Table.read(args.obslist)
+
+    obs = obslist[args.obsnum]
+
+    dateobs = astropy.time.Time(obs['MJD'], format='mjd')
+    night = dateobs.utc.isot[0:10].replace('-', '')     #- YEARMMDD string
+
+    if args.outdir is None:
+        args.outdir = desisim.io.simdir(night=night, mkdir=True)
+
+    if args.nspec is None:
+        args.nspec = len(fiberassign)
+
+    sim, meta = newexp(fiberassign, args.expid, night, args.mockdir,
+        obsconditions=obs, outsimdir=args.outdir, nspec=args.nspec)
