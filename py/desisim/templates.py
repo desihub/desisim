32732990ac1df51597600e90eff5ee58719130bf
John Moustakas <john.moustakas@gmail.com>
2017-09-07 14:17:57 -0400

    updated colorcuts_function documentation

--- py/desisim/templates.py
+++ py/desisim/templates.py
@@ -262,7 +262,10 @@ class GALAXY(object):
           wave (numpy.ndarray): Input/output observed-frame wavelength array,
             overriding the minwave, maxwave, and cdelt arguments (Angstrom).
           colorcuts_function (function name): Function to use to select
-            templates that pass the color-cuts for the specified objtype
+            templates that pass the color-cuts for the specified objtype Note
+            that this argument can also be a tuple of more than one selection
+            function to apply (e.g., desitarget.cuts.isBGS_faint and
+            desitarget.cuts.isBGS_bright) which will be applied in sequence
             (default None).
           normfilter (str): normalize each spectrum to the magnitude in this
             filter bandpass (default 'decam2014-r').
@@ -709,6 +712,8 @@ class GALAXY(object):
                 if nocolorcuts or self.colorcuts_function is None:
                     colormask = np.repeat(1, nbasechunk)
                 else:
+                    import pdb ; pdb.set_trace()
+                    
                     colormask = self.colorcuts_function(
                         gflux=synthnano['decam2014-g'],
                         rflux=synthnano['decam2014-r'],
@@ -869,11 +874,9 @@ class BGS(GALAXY):
 
         """
         if colorcuts_function is None:
-            try:
-                from desitarget.cuts import isBGS_bright as colorcuts_function
-            except:
-                from desitarget.cuts import isBGS as colorcuts_function
-                log.warning('You are using on old version of desitarget')
+            from desitarget.cuts import isBGS_bright
+            from desitarget.cuts import isBGS_faint
+            colorcuts_function = (isBGS_bright, isBGS_faint)
 
         super(BGS, self).__init__(objtype='BGS', minwave=minwave, maxwave=maxwave,
                                   cdelt=cdelt, wave=wave, colorcuts_function=colorcuts_function,
