eefff98c197ee18200c705473ced118c0053d2b4
John Moustakas <john.moustakas@gmail.com>
2017-11-28 12:41:15 -0500

    build 56 archetypes from 1000 templates

--- py/desisim/archetypes.py
+++ py/desisim/archetypes.py
@@ -11,13 +11,10 @@ from __future__ import absolute_import, division, print_function
 import os
 import numpy as np
 
-import multiprocessing
-nproc = multiprocessing.cpu_count() // 2
-
 from desiutil.log import get_logger
 log = get_logger()
 
-def compute_chi2(flux):
+def compute_chi2(flux, ferr=None):
     """Compute the chi2 distance matrix.
 
     Parameters
@@ -25,6 +22,8 @@ def compute_chi2(flux):
     flux : numpy.ndarray
         Array [Nspec, Npix] of spectra or templates where Nspec is the number of
         spectra and Npix is the number of pixels.
+    ferr : numpy.ndarray
+        Uncertainty spectra ccorresponding to flux (default None).
 
     Returns
     -------
@@ -38,47 +37,83 @@ def compute_chi2(flux):
     from SetCoverPy import mathutils    
 
     nspec, npix = flux.shape
-    ferr = np.ones_like(flux)
-    
-    chi2 = np.zeros((nspec, nspec))
-    amp = np.zeros((nspec, nspec))
+    if ferr is None:
+        ferr = np.ones_like(flux)
+
+    chi2 = np.zeros((nspec, nspec)).astype('f4')
+    amp = np.zeros((nspec, nspec)).astype('f4')
+
     for ii in range(nspec):
         if ii % 500 == 0 or ii == 0:
             log.info('Computing chi2 matrix for spectra {}-{} out of {}.'.format(
-                ii*500, np.min(((ii+1)*500, nspec)), nspec))
+                ii*500, np.min(((ii+1)*500, nspec-1)), nspec))
         xx = flux[ii, :].reshape(1, npix)
         xxerr = ferr[ii, :].reshape(1, npix)
         amp1, chi21 = mathutils.quick_amplitude(xx, flux, xxerr, ferr)
         chi2[ii, :] = chi21
         amp[ii, :] = amp1
-    
+
     return chi2, amp
 
 class ArcheTypes(object):
     """Object for generating archetypes and determining their responsibility.
 
+    Parameters
+    ----------
+    chi2 : numpy.ndarray
+        Chi^2 matrix computed by desisim.archetypes.compute_chi2().
+    
     """
-    def __init__(self):
-        pass
+    def __init__(self, chi2):
 
-    def get_archetypes():
-        """Get the final set of archetypes (and their responsibility, sorted by D4000)
-        by solving the SCP problem.
-        
+        self.chi2 = chi2
+
+    def get_archetypes(self, chi2_thresh=0.1, responsibility=False):
+        """Solve the SCP problem to get the final set of archetypes and, optionally,
+        their responsibility.
+
+        Note: We assume that each template has uniform "cost" but a more general
+        model in principle could be used / implemented.
+
+        Parameters
+        ----------
+        chi2 : numpy.ndarray
+            Chi^2 matrix computed by archetypes.compute_chi2().
+        chi2_thresh : float
+            Threshold chi2 value to differentiate "different" templates.
+        responsibility : bool
+            If True, then compute and return the responsibility of each archetype. 
+
+        Returns
+        -------
+        If responsibility==True then returns a tuple of (iarch, resp, respindx) where:
+            iarch : integer numpy.array
+                Indices of the archetypes [N].
+            resp : integer numpy.array
+                Responsibility of each archetype [N].
+            respindx : list of 
+                Indices the parent sample each archetype is responsible for [N].
+
+        If responsibility==False then only iarch is returned.
+
         """
+        from SetCoverPy import setcover
+
+        nspec = self.chi2[0].shape
         cost = np.ones(nspec) # uniform cost
-        a_matrix = (chi2 <= chi2_thresh) * 1
+        
+        a_matrix = (self.chi2 <= chi2_thresh) * 1
         gg = setcover.SetCover(a_matrix, cost)
         sol, time = gg.SolveSCP()
         
         iarch = np.nonzero(gg.s)[0]
-        iarch = iarch[np.argsort(meta['D4000'][iarch])]
-        nnarch = len(iarch)
-        resp, respindx = responsibility(iarch, a_matrix)
-    
-        return iarch, resp, respindx
-
-    def responsibility(iarch, a_matrix):
+        if responsibility:
+            resp, respindx = self.responsibility(iarch, a_matrix)
+            return iarch, resp, respindx
+        else:
+            return iarch
+  
+    def responsibility(self, iarch, a_matrix):
         """Method to determine the responsibility of each archetype.
     
         In essence, the responsibility is the number of templates described by each
@@ -86,8 +121,8 @@ class ArcheTypes(object):
         
         Parameters
         ----------
-          iarch : indices of the archetypes
-          a_matrix : distance matrix
+            iarch : indices of the archetypes
+            a_matrix : distance matrix
         
         Returns
         -------
@@ -98,10 +133,11 @@ class ArcheTypes(object):
         narch = len(iarch)
         resp = np.zeros(narch).astype('int16')
         respindx = []
+
         for ii, this in enumerate(iarch):
             respindx.append(np.where(a_matrix[:, this] == 1)[0])
             resp[ii] = np.count_nonzero(a_matrix[:, this])
             
-        return resp, np.array(respindx)
+        return resp, respindx
 
 
