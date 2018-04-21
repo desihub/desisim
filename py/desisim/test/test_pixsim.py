d1913903d965bc306feca5d2a13774c51994d96d
Stephen Bailey <stephen.code@gmail.com>
2018-04-20 17:27:00 -0700

    drop py2.7 support b/c math.gcd doesn't exist

--- py/desisim/test/test_pixsim.py
+++ py/desisim/test/test_pixsim.py
@@ -20,11 +20,6 @@ log = get_logger()
 desi_templates_available = 'DESI_ROOT' in os.environ
 desi_root_available = 'DESI_ROOT' in os.environ
 
-#- Travis tests hang when some of these tests are called with
-#- python 2.7, but not others.  Works away from Travis on py2.7.
-#- Skip for now.
-travis27 = (sys.version_info.major==2) and ('TRAVIS' in os.environ)
-
 class TestPixsim(unittest.TestCase):
     #- Create test subdirectory
     @classmethod
@@ -166,7 +161,6 @@ class TestPixsim(unittest.TestCase):
     #- called, though the tests work on other systems.
     #- Disabling multiprocessing also "fixed" this for unknown reasons.
     @unittest.skipIf(False, 'Skip test that is causing coverage tests to hang.')
-    @unittest.skipIf(travis27, 'Skip test that is causing Travis to hang on py2.7')
     def test_main_defaults(self):
         night = self.night
         expid = self.expid
@@ -200,7 +194,6 @@ class TestPixsim(unittest.TestCase):
         os.remove(simpixfile)
         os.remove(rawfile)
 
-    @unittest.skipIf(travis27, 'Skip test that is causing Travis to hang on py2.7')
     def test_main_override(self):
         night = self.night
         expid = self.expid
