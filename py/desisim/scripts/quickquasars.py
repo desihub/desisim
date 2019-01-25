104bd0e21bb6f0fb8f4b0374adc2bc88613ec05c
alxogm <alxogm@github.com>
2019-01-25 10:31:10 -0800

    fix space

--- py/desisim/scripts/quickquasars.py
+++ py/desisim/scripts/quickquasars.py
@@ -249,8 +249,8 @@ def simulate_one_healpix(ifilename,args,model,obsconditions,decam_and_wise_filte
     # might add metal transmission as well (from the HDU file).
     log.info("Read transmission file {}".format(ifilename))
     trans_wave, transmission, metadata, dla_info = read_lya_skewers(ifilename,read_dlas=(args.dla=='file'),add_metals=args.metals_from_file)   
-    ### Add Finger-of-God, before generate the continua
 
+    ### Add Finger-of-God, before generate the continua
     log.info("Add FOG to redshift with sigma {} to quasar redshift".format(args.sigma_kms_fog))
     DZ_FOG = args.sigma_kms_fog/c*(1.+metadata['Z'])*np.random.normal(0,1,metadata['Z'].size)
     metadata['Z'] += DZ_FOG
