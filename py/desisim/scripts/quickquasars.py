d078e78ad8f114a289dba204de3516234ac52795
alxogm <alxogm@github.com>
2019-03-19 18:25:10 -0700

    Fixed a missing comma.

--- py/desisim/scripts/quickquasars.py
+++ py/desisim/scripts/quickquasars.py
@@ -82,7 +82,7 @@ def parse(options=None):
     parser.add_argument('--no-simqso',action = "store_true", help="Does not use desisim.templates.SIMQSO to generate templates, and uses desisim.templates.QSO instead.")
     parser.add_argument('--no-transmission',action = 'store_true', help='Do not multiply continuum by transmission, use F=1 everywhere')
     parser.add_argument('--eboss',action = 'store_true', help='Setup footprint, number density, redshift distribution, and exposure time to generate eBOSS-like mocks')
-    parser.add_argument('--Rv',type=float,default=3.1,help='Adds Galactic extintion, for the specified extintion factor.)
+    parser.add_argument('--Rv',type=float,default=3.1,help='Adds Galactic extintion, for the specified extintion factor.')
     parser.add_argument('--no-extintion',action='store_true',help="Does not add galactic extintion")
 
     if options is None:
