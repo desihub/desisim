88540d9d3b63773f08d9115f474910fa36b89232
Benjamin Alan Weaver <baweaver@lbl.gov>
2019-09-30 16:29:45 -0700

    add speclite

--- doc/conf.py
+++ doc/conf.py
@@ -134,8 +134,8 @@ napoleon_include_private_with_doc = True
 # some external dependencies are not met at build time and break the
 # building process.
 autodoc_mock_imports = []
-for missing in ('astropy', 'desimodel', 'desiutil', 'desispec', 'desitarget', 'healpy',
-                'matplotlib', 'numpy', 'scipy', 'specsim', 'fitsio', 'yaml'):
+for missing in ('astropy', 'desimodel', 'desiutil', 'desispec', 'desitarget', 'fitsio',
+                'healpy', 'matplotlib', 'numpy', 'scipy', 'speclite', 'specsim', 'yaml'):
     try:
         foo = import_module(missing)
     except ImportError:
